88708bacf10985c288928ec876792966e3d60290
Felipe Coury <felipe.coury@gmail.com>
2025-02-18 15:02:47 -0300

    feat: improved service discovery

--- d30/src/bluetooth/macos.rs
+++ d30/src/bluetooth/macos.rs
@@ -14,11 +14,6 @@ use super::BluetoothSocket;
 #[link(name = "IOBluetooth", kind = "framework")]
 extern "C" {}
 
-// UUID for Serial Port Profile
-const SPP_UUID: &str = "00001101-0000-1000-8000-00805F9B34FB";
-// UUID for Serial Port Profile Service Class
-const SPP_SERVICE_CLASS_UUID: &str = "00001101-0000-1000-8000-00805F9B34FB";
-
 pub struct MacOSBluetoothSocket {
     device: id,
     channel: id,
@@ -30,9 +25,10 @@ impl BluetoothSocket for MacOSBluetoothSocket {
             debug!("Connecting to device with address: {}", addr);
             let pool: id = msg_send![class!(NSAutoreleasePool), new];
 
-            // Format MAC address with hyphens for IOBluetooth
+            // Format MAC address for IOBluetooth
             let addr = addr.replace(":", "-");
             let addr_str = NSString::alloc(nil).init_str(&addr);
+            debug!("Using formatted address: {}", addr);
 
             let device_class = match Class::get("IOBluetoothDevice") {
                 Some(class) => class,
@@ -44,8 +40,8 @@ impl BluetoothSocket for MacOSBluetoothSocket {
                 }
             };
 
-            // Get the device
-            self.device = msg_send![device_class, withAddressString:addr_str];
+            // Get device
+            self.device = msg_send![device_class, deviceWithAddressString:addr_str];
             if self.device == nil {
                 let _: () = msg_send![pool, release];
                 return Err(BluetoothError::ConnectionFailed(
@@ -53,15 +49,30 @@ impl BluetoothSocket for MacOSBluetoothSocket {
                 ));
             }
 
-            // Force fresh service discovery
+            debug!("Got device, checking name...");
+            let name: id = msg_send![self.device, name];
+            if name != nil {
+                let name_str: id = msg_send![name, UTF8String];
+                debug!("Device name: {:?}", name_str);
+            }
+
+            // Force service discovery
             debug!("Performing service discovery...");
-            let _: bool = msg_send![self.device, performSDPQuery:nil];
+            let inquiry_result: bool = msg_send![self.device, performSDPQuery:nil];
+            debug!("SDP query result: {}", inquiry_result);
             thread::sleep(Duration::from_millis(1000));
 
-            // Connect if not already connected
+            // Check device properties
+            let is_paired: bool = msg_send![self.device, isPaired];
             let is_connected: bool = msg_send![self.device, isConnected];
+            debug!(
+                "Device status - Paired: {}, Connected: {}",
+                is_paired, is_connected
+            );
+
+            // Try to connect if not already connected
             if !is_connected {
-                debug!("Device not connected, attempting to connect...");
+                debug!("Attempting to connect...");
                 let connect_result: bool = msg_send![self.device, openConnection];
                 if !connect_result {
                     let _: () = msg_send![pool, release];
@@ -72,64 +83,66 @@ impl BluetoothSocket for MacOSBluetoothSocket {
                 thread::sleep(Duration::from_millis(1000));
             }
 
-            // Get services
-            debug!("Looking for Serial Port Profile service...");
+            // Get all services
+            let mut services: id = msg_send![self.device, services];
+            if services == nil {
+                debug!("No services found, trying SDP query again...");
+                let _: bool = msg_send![self.device, performSDPQuery:nil];
+                thread::sleep(Duration::from_millis(1000));
+                services = msg_send![self.device, services];
+            }
 
-            // Get the CBUUID class and create UUID object
-            let uuid_class = match Class::get("CBUUID") {
-                Some(class) => class,
-                None => {
-                    let _: () = msg_send![pool, release];
-                    return Err(BluetoothError::InternalError(
-                        "CBUUID class not found".to_string(),
-                    ));
+            if services == nil {
+                let _: () = msg_send![pool, release];
+                return Err(BluetoothError::ConnectionFailed(
+                    "No services available".to_string(),
+                ));
+            }
+
+            let count: usize = msg_send![services, count];
+            debug!("Found {} services", count);
+
+            // Inspect each service
+            for i in 0..count {
+                let service: id = msg_send![services, objectAtIndex:i];
+                if service != nil {
+                    // Try to get service UUID
+                    let uuid: id = msg_send![service, getServiceUUID];
+                    if uuid != nil {
+                        let uuid_str: id = msg_send![uuid, UUIDString];
+                        debug!("Service {}: UUID = {:?}", i, uuid_str);
+                    }
+
+                    // Try to get RFCOMM channel
+                    let mut channel_id: u8 = 0;
+                    let has_channel: bool = msg_send![service, getRFCOMMChannelID:&mut channel_id];
+                    if has_channel {
+                        debug!("Service {} has RFCOMM channel: {}", i, channel_id);
+
+                        // Try to open this channel
+                        let mut rfcomm_channel: id = nil;
+                        debug!("Attempting to open channel {}...", channel_id);
+                        let result: bool = msg_send![self.device,
+                                                   openRFCOMMChannelSync:&mut rfcomm_channel
+                                                   withChannelID:channel_id
+                                                   delegate:nil];
+
+                        if result && rfcomm_channel != nil {
+                            debug!("Successfully opened channel {}", channel_id);
+                            self.channel = rfcomm_channel;
+                            let _: () = msg_send![pool, release];
+                            return Ok(());
+                        } else {
+                            debug!("Failed to open channel {}", channel_id);
+                        }
+                    }
                 }
-            };
-
-            let spp_uuid = NSString::alloc(nil).init_str(SPP_UUID);
-            let uuid_obj: id = msg_send![uuid_class, UUIDWithString:spp_uuid];
-
-            // Get service with SPP UUID
-            let service: id = msg_send![self.device, getServiceRecordForUUID:uuid_obj];
-            if service == nil {
-                let _: () = msg_send![pool, release];
-                return Err(BluetoothError::ConnectionFailed(
-                    "Serial Port Profile service not found".to_string(),
-                ));
             }
-
-            // Get RFCOMM channel from service
-            let mut channel_id: u8 = 0;
-            let result: bool = msg_send![service, getRFCOMMChannelID:&mut channel_id];
-            if !result {
-                let _: () = msg_send![pool, release];
-                return Err(BluetoothError::ConnectionFailed(
-                    "Could not get RFCOMM channel ID".to_string(),
-                ));
-            }
-
-            debug!("Found RFCOMM channel ID: {}", channel_id);
-
-            // Open the RFCOMM channel
-            let mut rfcomm_channel: id = nil;
-            let result: bool = msg_send![self.device,
-                                       openRFCOMMChannelSync:&mut rfcomm_channel
-                                       withChannelID:channel_id
-                                       delegate:nil];
-
-            if !result || rfcomm_channel == nil {
-                let _: () = msg_send![pool, release];
-                return Err(BluetoothError::ConnectionFailed(format!(
-                    "Failed to open RFCOMM channel {}",
-                    channel_id
-                )));
-            }
-
-            self.channel = rfcomm_channel;
 
             let _: () = msg_send![pool, release];
-            debug!("Successfully connected to device on channel {}", channel_id);
-            Ok(())
+            Err(BluetoothError::ConnectionFailed(
+                "Could not find usable RFCOMM channel".to_string(),
+            ))
         }
     }
 
